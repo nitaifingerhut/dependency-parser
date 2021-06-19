f004895ab0a2e09ef0a0c505c73660c4b95b2ffa
daniel-ablin <daniel.avlin@gmail.com>
2021-06-19 19:50:54 +0300

    Create train6.sh

---
+++ scripts/train6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Setup env
+cd ..
+conda activate nlp_hw2
+echo "hello from $(python --version) in $(which python)"
+echo "(nlp_hw2) training starts"
+cd dependency-parser
+
+# Run the experiments
+python train.py --num-epochs 35 --batch-size 32  --optimizer Adam --lr 0.0001 --optimizer-params betas=0.9,0.99 --name 19-6_b_032_lr_0.0001_base_new
+git add -A
+git commit -m "auto..."
+git push
