6bcf1a061bd50a2f2f8863623de5f17052f1c097
nitaifingerhut <nitaifingerhut@gmail.com>
2021-06-20 18:39:39 +0300

    parser with transformer

--- models/dependency_parser.py
+++ models/dependency_parser.py
@@ -1,6 +1,7 @@
 from models.dependency_parser_v1 import DependencyParserV1
-
+from models.dependency_parser_v2 import DependencyParserV2
 
 MODELS = {
     "DependencyParserV1": DependencyParserV1,
+    "DependencyParserV2": DependencyParserV2,
 }
