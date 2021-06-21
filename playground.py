6744b8e55c4d0dcbeb7d1d845791e683ccf6b158
nitaifingerhut <nitaifingerhut@gmail.com>
2021-06-21 18:30:07 +0300

    base candidates

---
+++ playground.py
@@ -0,0 +1,20 @@
+import json
+from pathlib import Path
+
+
+if __name__ == "__main__":
+
+    path = Path("checkpoints")
+
+    results = {}
+    for dir in path.iterdir():
+        if not dir.is_dir():
+            continue
+
+        with open(dir.joinpath("params.json"), "r") as f:
+            data = json.load(f)
+
+        results[data["test_max_accuracy"]["value"]] = dir.stem
+
+    s = [str(k) + "@" + v for k, v in results.items()]
+    print("\n".join(s))
